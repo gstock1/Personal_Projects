6667658865ebb0e4d9c8f0bf1ff9f604af2285c8
George S <georgesbulls@gmail.com>
2021-04-16 14:54:37 -0500

    comitting website

---
+++ website/DjangoProjects/PersonalWebsite/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\georg\Desktop\School\Personal_Projects\WEBSITE\DjangoProjects\PersonalWebsite\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
